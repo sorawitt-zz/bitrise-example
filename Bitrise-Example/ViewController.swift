18cdd82990b512d238386536786bd13a4fd63207
Sorawit Trutsat <sorawit.tr@gmail.com>
2019-08-17 14:50:38 +0700

    Initial Commit

---
+++ Bitrise-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bitrise-Example
+//
+//  Created by Sorawit Trutsat on 17/8/2562 BE.
+//  Copyright © 2562 Sorawit Trutsat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
